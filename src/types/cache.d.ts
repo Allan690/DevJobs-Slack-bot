0b218ab9df862f5c6eb13a62c8e6ac6d2e5691c4
AitorDB <aitor.db.mail@gmail.com>
2019-10-14 03:28:52 +0100

    Update redis module for custom methods

--- src/types/cache.d.ts
+++ src/types/cache.d.ts
@@ -1,1 +1,11 @@
-declare module '*';
+import redis from 'redis';
+
+declare module 'redis' {
+  export interface RedisClient {
+    getAsync (key: string): Promise<any>;
+    setAsync (key: string, value: any): Promise<any>;
+    setexAsync (key: string, second: number, value: any): Promise<any>;
+    delAsync (key: string): Promise<any>;
+    flushallAsync (): Promise<any>;
+  }
+}
